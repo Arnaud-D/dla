26ba90164f4bf4e2cd8f30158d697bf3ab6f7fc4
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-10-30 18:16:02 +0100

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,74 @@
 use image;
 use rand;
 use rand::{Rng, SeedableRng};
+use rand::seq::SliceRandom;
+
+fn has_no_neighbours(matrix : &Vec<Vec<bool>>, x : usize, y: usize) -> bool {
+    matrix[x + 1][y] && matrix[x - 1][y] && matrix[x][y + 1] && matrix[x][y - 1]
+}
+
+fn generate_matrix(size: usize, n_walks: u32) -> Vec<Vec<bool>> {
+    let mut matrix = vec![vec![true; size]; size];
+    let midpoint = (size + 1) / 2;
+    matrix[midpoint][midpoint] = false;
+
+    let mut rg_pixels = rand::rngs::SmallRng::from_entropy();
+    let mut rg_neighbours = rand::rngs::SmallRng::from_entropy();
+    let mut x;
+    let mut y;
+    let neighbour_coords= [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    let idx_min = 1;
+    let idx_max = size - 2;
+    for n in 0..n_walks {
+        x = rg_pixels.gen_range(idx_min, idx_max);
+        y = rg_pixels.gen_range(idx_min, idx_max);
+        while has_no_neighbours(&matrix, x, y) {
+            let coord = neighbour_coords.choose(&mut rg_neighbours).unwrap();
+
+            x = (x as i32 + coord.0) as usize;
+            if x > idx_max {
+                x = idx_max;
+            } else if x < idx_min {
+                x = idx_min;
+            }
+
+            y = (y as i32 + coord.1) as usize;
+            if y > idx_max {
+                y = idx_max;
+            } else if y < idx_min {
+                y = idx_min;
+            }
+        }
+        matrix[x][y] = false;
+        println!("Random walk {}/{}.", n + 1, n_walks);
+    }
+    return matrix;
+}
+
+fn convert_matrix(matrix: Vec<Vec<bool>>) -> image::GrayImage {
+    let width = matrix.len();
+    let height = matrix[0].len();
+    let mut img = image::GrayImage::new(width as u32, height as u32);
+    let hot_pixel = image::Luma([255]);
+    for x in 0..width {
+        for y in 0..height {
+            if !matrix[x][y] {
+                img.put_pixel(x as u32, y as u32, hot_pixel);
+            }
+        }
+    }
+    return img;
+}
 
 fn main() {
     let output_filename = "test.png";
     let img_size = 501;
-    let n_walks = 1000;
+    let n_walks = 10000;
 
     let start_time = std::time::SystemTime::now();
 
-    let mut img = image::GrayImage::new(img_size, img_size);
-    let midpoint = (img_size + 1) / 2;
-    let hot_pixel = image::Luma([255]);
-
-    img.put_pixel(midpoint, midpoint, hot_pixel);
-    let mut rg_pixels = rand::rngs::SmallRng::from_entropy();
-    let mut rg_neighbours = rand::rngs::SmallRng::from_entropy();
-    let mut x:i32;
-    let mut y:i32;
-    let dx: [i32; 4] = [1, -1, 0, 0];
-    let dy: [i32; 4] = [0, 0, 1, -1];
-    for n in 0..n_walks {
-        x = rg_pixels.gen_range(1, img_size - 2) as i32;
-        y = rg_pixels.gen_range(1, img_size - 2) as i32;
-        while !(img.get_pixel((x + 1) as u32, y as u32) == &hot_pixel
-            || img.get_pixel((x - 1) as u32, y as u32) == &hot_pixel
-            || img.get_pixel(x as u32, (y + 1) as u32) == &hot_pixel
-            || img.get_pixel(x as u32, (y - 1) as u32) == &hot_pixel
-        ) {
-            let neighbour_idx = rg_neighbours.gen_range(0, 4);
-
-            x = x + dx[neighbour_idx];
-            if x > 499 {
-                x = 499;
-            } else if x < 1 {
-                x = 1;
-            }
-
-            y = y + dy[neighbour_idx];
-            if y > 499 {
-                y = 499;
-            } else if y < 1 {
-                y = 1;
-            }
-        }
-        img.put_pixel(x as u32, y as u32, hot_pixel);
-        println!("Random walk {}/{}.", n + 1, n_walks)
-    }
+    let matrix = generate_matrix(img_size, n_walks);
+    let img = convert_matrix(matrix);
 
     let elapsed = match start_time.elapsed() {
         Ok(s) => s,
