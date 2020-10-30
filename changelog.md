bb1efd9cf531e58a982dd9f6093c609cbbf91a0e
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2020-10-30 19:24:54 +0100

    Change image colors

--- changelog.md
+++ changelog.md
@@ -4,6 +4,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+
+## [Unreleased]
+### Added
+- Restart random walk when particle escapes
+
+### Changed
+- New image colors
+
+
 ## [v0.1.0] - 2020-30-10
 ### Added
 - Add license
--- src/main.rs
+++ src/main.rs
@@ -44,11 +44,14 @@ fn convert_matrix(matrix: Vec<Vec<bool>>) -> image::GrayImage {
     let width = matrix.len();
     let height = matrix[0].len();
     let mut img = image::GrayImage::new(width as u32, height as u32);
-    let hot_pixel = image::Luma([255]);
+    let hot_pixel = image::Luma([0]);
+    let cold_pixel = image::Luma([255]);
     for x in 0..width {
         for y in 0..height {
             if !matrix[x][y] {
                 img.put_pixel(x as u32, y as u32, hot_pixel);
+            } else {
+                img.put_pixel(x as u32, y as u32, cold_pixel);
             }
         }
     }
